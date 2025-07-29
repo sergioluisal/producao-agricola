276c41ef9de989fbd2be20450917ac0cc46ccbc8
sergioluisal <sergioluisal@gmail.com>
2025-07-29 10:35:57 -0300

    Update streamlit_dashboard.py

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -892,7 +892,6 @@ st.markdown('</div>', unsafe_allow_html=True)
 
 # Footer
 st.markdown("---")
-st.markdown(
     """
     <div style='text-align: center; color: #666;'>
         <p>🌾 Dashboard de Análise Agrícola | Desenvolvido por Sérgio</p>
